68271c929f8362408aa21dd9c1a14b4b7383cf19
vaishnavi-bahirat <vaishnavibahirat.86@gmail.com>
2025-04-18 17:03:54 +0530

    add timestamp

--- React/react/src/components/NoteItem.jsx
+++ React/react/src/components/NoteItem.jsx
@@ -6,6 +6,7 @@ const NoteItem = ({ note, deleteNote, startEditing }) => {
       <div className="card-body">
         <h5 className="card-title">{note.title}</h5>
         <p className="card-text">{note.content}</p>
+        <small>{new Date(note.createdAt).toLocaleString()}</small>
         <div className="d-flex justify-content-end">
   <button className="btn btn-sm btn-secondary me-2" onClick={() => startEditing(note)}>Edit</button>
   <button className="btn btn-sm btn-danger" onClick={() => deleteNote(note._id)}>Delete</button>
